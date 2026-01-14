e9f3483793968985a666ddd0e51c170ed2ca52a7
geeeemi8 <gemesva@gmail.com>
2026-01-14 11:24:32 +0100

    Update frontend API URL to deployed backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8000';
+const API_BASE_URL = 'https://sp-project-group-d-1.onrender.com';
+
 
 const api = axios.create({
   baseURL: API_BASE_URL,
